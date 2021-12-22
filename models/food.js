6a03ea50f715f2e570e1748717414e8f42a11aa6
mcodemax <mwalterjohnson7@gmail.com>
2021-12-21 23:35:09 -0800

    more comments and reformatting f() of food.js

--- models/food.js
+++ models/food.js
@@ -15,36 +15,91 @@ const FDC_BASE_URL = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=`
 
 //.then(e => console.log(e));
 
-//list 8 nutrients
-
 //in a query param replace spaces with %20
 
 //play with FDC API https://app.swaggerhub.com/apis/fdcnal/food-data_central_api/1.0.0#/FDC/postFoodsSearch
-async function name1(food) {
+
+
+/** Search for foods then return then in a more readable format
+ * most likely better to implement in UI.
+ * 
+ * For backend:
+ *  -In UI client calls searchFdcApi
+ *  -another f() calls /v1/food/{fdcId} and only adds the {fdcId} to the plate
+ *  -then when the UI calls the plate model, multiple fdc api calls can be made,
+ *  -populate the UI, and the UI can do all the logic to add up the nutrients.
+ * 
+ *  ^For above possibly have a picture link stored in  above plate's table schema;
+ *      -so another API call can be made to  pexels
+ */
+async function searchFdcApi(foodQuery) {
     const query = {
-        "query": `${food}`,
+        "query": `${foodQuery}`,
         "pageSize": 25,
         "pageNumber": 1,
         "sortBy": "dataType.keyword",
         "sortOrder": "asc"
     }
-    // const response = await axios.post(`${FDC_BASE_URL}${FDC_BASE_URL}`, query);
-
-    // return response.data;
+    const nutrientIdsArr = [1008, 1004, 1003, 1087, 1162, 1079, 1005];
+    
     //https://axios-http.com/docs/post_example
 
-    console.log('url', `${FDC_BASE_URL}${FDC_BASE_URL}`);
+    try {
+        const res = await axios.post(`${FDC_BASE_URL}${SECRET_API_KEY}`, query);
+        const foods = res.data.foods;
+        const foodArr = [];
 
-    axios.post(`${FDC_BASE_URL}${SECRET_API_KEY}`, query)
-        .then((response) => {
-         console.log(response.data);
-        }).catch( error => {
-            if( error.response ){
-                console.log(error.response.data); // => the response payload 
+        if(!foods.length) return `No foods found`;
+        //later: if no foods found returned; show it on the UI
+        
+        //takes food data response and filters into actually needed data.
+        for(food of foods){
+            // console.log(food.description)
+            const foodObj = {
+                "fdcId": food.fdcId,
+                "description": food.description,
+                "lowercaseDescription": food.lowercaseDescription,
+                "dataType": food.dataType,
+                "publishedDate": food.publishedDate,
+                "brandOwner": food.brandOwner,
+                "brandName": food.brandName,
+                "ingredients": food.ingredients,
+                "marketCountry": food.marketCountry,
+                "foodCategory": food.foodCategory,
+                "modifiedDate": food.modifiedDate,
+                "dataSource": food.dataSource,
+                "packageWeight": food.packageWeight,
+                "servingSizeUnit": food.servingSizeUnit,
+                "servingSize": food.servingSize,
+                "score": food.score,
+            };
+            const nutrientsArr = [];
+
+            for(nutrient of food.foodNutrients){
+
+                if(nutrientIdsArr.includes(nutrient.nutrientId)){
+                    nutrientsArr.push(nutrient);
+                }
             }
-        });
-    
+            
+            foodObj.foodNutrients = nutrientsArr;
+            foodArr.push(foodObj);
+        }
+
+        return foodArr;
+    } catch (error) {
+        return new Error(`FDC API error. code: ${error.response.data.error.code},
+         message: ${error.response.data.error.message}`);
+    }
 }
+
+/** Gets the nutrients */
+function getWantedNutrients(array){
+    //for array of nutrients in an individual food
+    //
+
+}
+
 
 //node -i -e "$(< food.js)"
 
@@ -119,3 +174,7 @@ function dummy(){
 
     */
 }
+
+module.exports = {
+    searchFdcApi
+};
